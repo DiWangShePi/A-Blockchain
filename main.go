7999d47ecbaae7e6833cc4247aadcf864f8be447
DiWangShePi <1725375420@qq.com>
2024-10-25 21:13:10 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"github.com/DiWangShePi/A-Blockchain/network"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	trLocal := network.NewLocalTransport("LOCAL")
+	trRemote := network.NewLocalTransport("REMOTE")
+
+	trLocal.Connect(trRemote)
+	trRemote.Connect(trLocal)
+
+	go func() {
+		for {
+			trRemote.SendMessage(trLocal.Addr(), []byte("Hello World"))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	opts := network.ServerOpts{
+		Transports: []network.Transport{trLocal},
+	}
+
+	s := network.NewServer(opts)
+	s.Start()
 }
